9869b35fa26618c19baf1850e05648a8ccb0d959
Chrisnon <obichristianchinonso@gmail.com>
2023-03-17 20:03:04 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,6 +18,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %d byte(s)", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %d byte(s)", (unsigned long)sizeof(d));
-	printf("Size of a float: %d  byte(s)", (unsigned long)sizeof(f));
+	printf("Size of a float: %d byte(s)", (unsigned long)sizeof(f));
 	return (0);
 }
