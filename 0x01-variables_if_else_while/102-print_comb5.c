4ea6a3a44c084c607a708f50a75bdeb66c83ce49
Chrisnon <obichristianchinonso@gmail.com>
2023-03-17 21:41:49 -0700

    My last commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
- * main Entry point
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
 
+{
+
 	int p, q;
-	   
+
 	for (p = 0; p <= 98; p++)
 	{
-
 	for (q = p + 1; q <= 99; q++)
 	{
-	
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
 	putchar(' ');
@@ -29,5 +28,6 @@ int main(void)
 	putchar(' ');
 	}
 	}
-        putchar('\n');
+	putchar ('\n');
 	return (0);
+}
